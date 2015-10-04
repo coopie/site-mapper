a7b56ea12e4aa30ff84d367c17752c1ae03050c9
Sam Coope <sam.j.coope@gmail.com>
2015-10-04 16:39:27 +0100

    Scraping a page for links working

--- src/crawler.js
+++ src/crawler.js
@@ -1,24 +1,23 @@
 var Promise = require('bluebird');
 var http = require('http');
 var htmlparser2 = require('htmlparser2');
+var Set = require('node-set');
+var _ = require('lodash');
 
-var handler = new htmlparser2.DefaultHandler(function(error, dom) {
-    console.log('dom : ', dom);
-});
+var handler = new htmlparser2.DefaultHandler();
 var parser = new htmlparser2.Parser(handler, {decodeEntries: true});
 
 function crawl(url) {
-
     return getData(url)
     .then(function(data) {
-        return parser.parseComplete(data.toString());
+        parser.parseComplete(data.toString());
+        return extractLinksFromDom(handler.dom);
     });
 }
 
 function getData(url) {
     return new Promise(function(fulfill, reject) {
         http.get(url, function(response) {
-
             response.on('data', function(data) {
                 fulfill(data);
             });
@@ -27,6 +26,24 @@ function getData(url) {
             reject(error);
         });
     });
+}
+
+function extractLinksFromDom(dom) {
+    return _.unique(extractLinksFromDomHelper(dom));
+
+    function extractLinksFromDomHelper(dom) {
+        return dom.reduce(function(links, domNode) {
+            if (domNode.name && domNode.name === 'a' &&
+                domNode.attribs && domNode.attribs.href) {
+                links.push(domNode.attribs.href);
+            }
+
+            if (domNode.children) {
+                links = links.concat(extractLinksFromDomHelper(domNode.children));
+            }
+            return links;
+        }, []);
+    }
 }
 
 module.exports = {
